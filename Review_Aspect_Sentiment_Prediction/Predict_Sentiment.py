14e88be3087d0de8f7db0dd6fad906644b07e363
Hiten1928 <hg1928@rit.edu>
2018-05-04 18:44:56 -0400

    Added funciton for data frame

--- Review_Aspect_Sentiment_Prediction/Predict_Sentiment.py
+++ Review_Aspect_Sentiment_Prediction/Predict_Sentiment.py
@@ -7,3 +7,8 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 
+ef create_sentiments_pandas_frame():
+    data = pd.read_csv("C:\\Users\ROSS\Documents\Study\Software Quality\Train_Set.csv")
+    processed_data = data.copy()
+    processed_data['sentiment'] = processed_data['Polarity'].apply(lambda x: 0 if x == 'negative' else 1)
+    return processed_data
