73206ca383825dd45e8d567ece1b034ef9f65b37
Hiten1928 <hg1928@rit.edu>
2018-05-04 18:42:31 -0400

    Added Prediction Sentiment imports

---
+++ Review_Aspect_Sentiment_Prediction/Predict_Sentiment.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+
