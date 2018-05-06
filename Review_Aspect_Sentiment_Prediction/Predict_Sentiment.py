5b2f9e0e415650700d93496e95d1aa270f35f077
Hiten1928 <hg1928@rit.edu>
2018-05-06 11:17:02 -0400

    test and train set created

--- Review_Aspect_Sentiment_Prediction/Predict_Sentiment.py
+++ Review_Aspect_Sentiment_Prediction/Predict_Sentiment.py
@@ -16,3 +16,19 @@ def create_sentiments_pandas_frame():
     processed_data['text_clean'] = processed_data['Text']
     processed_data = processed_data.loc[:, ['text_clean', 'sentiment']]
     return processed_data
+
+
+# Function to vectorize and split the data set
+def create_sentiments_test_train_set(processed_data):
+    X = processed_data['text_clean']
+    y = processed_data['sentiment']
+
+    # Using CountVectorizer to convert text into tokens/features
+    vectorizer = CountVectorizer(stop_words='english', ngram_range=(1, 1), max_df=.80, min_df=4)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.3)
+    # Using training data to transform text into counts of features for each message
+    vectorizer.fit(X_train)
+    X_train_dtm = vectorizer.transform(X_train)
+    X_test_dtm = vectorizer.transform(X_test)
+
+    return X, y, X_train, X_test, y_train, y_test, X_train_dtm, X_test_dtm, vectorizer
